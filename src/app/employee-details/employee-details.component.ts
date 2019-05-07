020d535eafaaa0b617b094e661568f6ed32f897b
Adrian Fâciu <adrian.faciu@gmail.com>
2019-05-07 09:40:07 +0300

    Extracting html and css

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -9,48 +9,8 @@ import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'app-employee-details',
-  template: `
-    <div *ngIf="employee$ | async as emp">
-      <mat-form-field>
-        <input matInput placeholder="Name" [value]="emp.name" />
-      </mat-form-field>
-
-      <mat-form-field>
-        <input matInput placeholder="Email" [value]="emp.email" />
-      </mat-form-field>
-
-      <mat-form-field>
-        <input matInput placeholder="Phone" [value]="emp.phone" />
-      </mat-form-field>
-
-      <mat-form-field>
-        <input matInput placeholder="Position" [value]="emp.position" />
-      </mat-form-field>
-
-      <mat-form-field>
-        <textarea matInput placeholder="Address">
-          {{ emp.address.street }}
-          {{ emp.address.number }}
-          {{ emp.address.city }}
-        </textarea
-        >
-      </mat-form-field>
-    </div>
-  `,
-  styles: [
-    `
-      div {
-        display: flex;
-        flex-direction: column;
-        width: 40%;
-        margin: 0 auto;
-      }
-
-      textarea {
-        min-height: 80px;
-      }
-    `,
-  ],
+  templateUrl: './employee-details.component.html',
+  styleUrls: ['./employee-details.component.css'],
 })
 export class EmployeeDetailsComponent {
   employee$: Observable<Employee>;
